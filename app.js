f6bf15a2b9acda28249bfc66dabc7b5958c7b552
dastroort <davidegarbato2@gmail.com>
2024-06-23 15:56:50 +0200

    Generalize a specific point in a PointND
    Points are now created in a common class. Point1D, Point2D, Point3D and Point4D are deleted.

--- app.js
+++ app.js
@@ -104,69 +104,43 @@ function RotationZW(angle){ return [
 }
 
 
-class Point1D{
-  constructor(x){
-    this.x = x;
+class PointND{
+  constructor(...coordinates){
+    this.nthDimension = coordinates.length;
+    this.coordinates = coordinates;
   }
-
-  toVector() {return [ [this.x] ]; }
-  static toPoint(vector) { return new Point1D(vector[0][0]); }
-
-  draw(width, depth){
-    context.beginPath();
-    context.arc(this.x + pointSize/2 + dims.width/2, pointSize/2 + dims.height/2, pointSize / (cameraDistance - width), 0, 2*Math.PI, false);
-    context.stroke();
-    context.strokeStyle = "rgba(0,0,0,0.25)";
-
-    const hue = 36 * depth + 270;
-    const color = `hsla(${hue}, 100%, 50%, ${250 / (cameraDistance - width)}%)`;
-
-    context.fillStyle = color;
-    context.fill();
-    context.closePath();
+  toVector() {
+    let vector = [];
+    for(let i=0; i<this.coordinates.length; i++) vector.push([this.coordinates[i]]);
+    return vector;
   }
-}
-
-class Point2D extends Point1D{
-  constructor(x, y){
-    super(x);
-    this.y = y;
+  static toPoint(vector){
+    for(let i=0; i<vector.length; i++) vector[i] = vector[i][0];
+    return new PointND(vector);
   }
-
-  toVector() { return [ [this.x], [this.y] ]; }
-  static toPoint(vector) { return new Point2D(vector[0][0], vector[1][0]); }
-
-  draw(depth, hyperdepth){
-    context.beginPath();
-    context.arc(this.x + pointSize/2 + dims.width/2, this.y + pointSize/2 + dims.height/2, pointSize / (cameraDistance - depth), 0, 2*Math.PI, false);
-    context.stroke();
-    context.strokeStyle = "rgba(0,0,0,0.25)";
-
-    const hue = 36 * hyperdepth + 270;
-    const color = `hsla(${hue}, 100%, 50%, ${250 / (cameraDistance - depth)}%)`;
-
-    context.fillStyle = color;
-    context.fill();
-    context.closePath();
-  }
-}
-class Point3D extends Point2D{
-  constructor(x, y, z){
-    super(x,y);
-    this.z = z;
-  }
-  toVector() { return [ [this.x], [this.y], [this.z] ]; }
-  static toPoint(vector) { return new Point3D(vector[0][0], vector[1][0], vector[2][0]); }
+  draw(depth, higherCoordinatesFactor=0){
+    // only a point in 2 dimensions can be drawn on a screen
+    if(this.nthDimension > 2) throw new Error("This point has too many dimensions to be drawn. You should project it");
+    if(this.nthDimension === 2){
+      context.beginPath();
+      let positionX = this.coordinates[0] + 5 + dims.width/2;
+      let positionY = this.coordinates[1] + 5 + dims.height/2;
+      let pointSize = 10 / (cameraDistance - depth);
+      context.arc(positionX, positionY, pointSize, 0, 2*Math.PI, false);
+      context.stroke();
+      context.strokeStyle = "rgba(0,0,0,0.25)";
+      // Calcola il colore basato su hyperdepth
+      // const hue = 36 * hyperdepth + 270; // Hue secondo i commenti
+      // const color = `hsla(${hue}, 100%, 50%, ${250 / (cameraDistance - depth)}%)`;
+      const color = `hsla(270, 9.8%, 80%, ${250 / (cameraDistance - depth)}%)`;
   
-  // other methods ...
-}
-class Point4D extends Point3D{
-  constructor(x, y, z, w){
-    super(x,y,z);
-    this.w = w;
+      // Applica il colore calcolato come riempimento
+      context.fillStyle = color;
+      context.fill();
+      context.closePath();
+    } else if(this.nthDimension === 1) (new PointND([...this.coordinates,0])).draw(depth);
+    else if(this.nthDimension === 0) (new PointND([0,0])).draw(depth);
   }
-  toVector() { return [ [this.x], [this.y], [this.z], [this.w] ]; }
-  static toPoint(vector) { return new Point4D(vector[0][0], vector[1][0], vector[2][0], vector[3][0]); }
 }
 
 const hypersphereRadius = 2;
@@ -181,14 +155,14 @@ function createCircle2D(radius, stepAngle, fixedCoordinates={"z": 0,"w": 0}) {
     let x = radius * Math.cos(theta);
     let y = radius * Math.sin(theta);
     if (fixedCoordinates.z !== undefined){
-      points.push(new Point4D(x, y, fixedCoordinates.z, fixedCoordinates.w));
+      points.push(new PointND(x, y, fixedCoordinates.z, fixedCoordinates.w));
     } else if (fixedCoordinates.y !== undefined){
       let z = y;
-      points.push(new Point4D(x, fixedCoordinates.y, z, fixedCoordinates.w));
+      points.push(new PointND(x, fixedCoordinates.y, z, fixedCoordinates.w));
     } else if (fixedCoordinates.x !== undefined){
       let z = y;
       y = x;
-      points.push(new Point4D(fixedCoordinates.x, y, z, fixedCoordinates.w));
+      points.push(new PointND(fixedCoordinates.x, y, z, fixedCoordinates.w));
     } else {
       throw new Error("invalid properties in \"fixedCordinates\"");
     }
@@ -225,11 +199,11 @@ function createSphere4D(radius, stepAngle, noOfSpheres=3) {
 
 function createSimplex4D(){
   return [
-    new Point4D(-1, -Math.sqrt(3)/3, -Math.sqrt(6)/6, -Math.sqrt(10)/10),
-    new Point4D(1, -Math.sqrt(3)/3, -Math.sqrt(6)/6, -Math.sqrt(10)/10),
-    new Point4D(0, 2*Math.sqrt(3)/3, -Math.sqrt(6)/6, -Math.sqrt(10)/10),
-    new Point4D(0, 0, Math.sqrt(6)/2, -Math.sqrt(10)/10),
-    new Point4D(0, 0, 0, 2*Math.sqrt(10)/5)
+    new PointND(-1, -Math.sqrt(3)/3, -Math.sqrt(6)/6, -Math.sqrt(10)/10),
+    new PointND(1, -Math.sqrt(3)/3, -Math.sqrt(6)/6, -Math.sqrt(10)/10),
+    new PointND(0, 2*Math.sqrt(3)/3, -Math.sqrt(6)/6, -Math.sqrt(10)/10),
+    new PointND(0, 0, Math.sqrt(6)/2, -Math.sqrt(10)/10),
+    new PointND(0, 0, 0, 2*Math.sqrt(10)/5)
   ];
 }
 
@@ -238,7 +212,7 @@ function createCube3D() {
   for (let i = -1; i <= 1; i += 2) {
     for (let j = -1; j <= 1; j += 2) {
       for (let k = -1; k <= 1; k += 2) {
-        vertices.push(new Point4D(i, j, k, 0));
+        vertices.push(new PointND(i, j, k, 0));
       }
     }
   }
@@ -251,7 +225,7 @@ function createCube4D() {
     for (let j = -1; j <= 1; j += 2) {
       for (let k = -1; k <= 1; k += 2) {
         for (let l = -1; l <= 1; l += 2) {
-          vertices.push(new Point4D(i, j, k, l));
+          vertices.push(new PointND(i, j, k, l));
         }
       }
     }
@@ -265,20 +239,20 @@ function create24Cell() {
   const coords = [1, -1];
   for (let i of coords) {
     for (let j of coords) {
-      vertices.push(new Point4D(i, j, 0, 0));
-      vertices.push(new Point4D(i, 0, j, 0));
-      vertices.push(new Point4D(i, 0, 0, j));
-      vertices.push(new Point4D(0, i, j, 0));
-      vertices.push(new Point4D(0, i, 0, j));
-      vertices.push(new Point4D(0, 0, i, j));
+      vertices.push(new PointND(i, j, 0, 0));
+      vertices.push(new PointND(i, 0, j, 0));
+      vertices.push(new PointND(i, 0, 0, j));
+      vertices.push(new PointND(0, i, j, 0));
+      vertices.push(new PointND(0, i, 0, j));
+      vertices.push(new PointND(0, 0, i, j));
     }
   }
   // Generate vertices of the form (±1, 0, 0, 0)
   for (let i of coords) {
-    vertices.push(new Point4D(i, 0, 0, 0));
-    vertices.push(new Point4D(0, i, 0, 0));
-    vertices.push(new Point4D(0, 0, i, 0));
-    vertices.push(new Point4D(0, 0, 0, i));
+    vertices.push(new PointND(i, 0, 0, 0));
+    vertices.push(new PointND(0, i, 0, 0));
+    vertices.push(new PointND(0, 0, i, 0));
+    vertices.push(new PointND(0, 0, 0, i));
   }
   return vertices;
 }
@@ -307,16 +281,16 @@ function tic(){
   vertices = vertices.sort((a,b) => b.z - a.z)
   for(let i=0; i<vertices.length; i++){
     let rotated = vertices[i];
-    rotated = Point4D.toPoint(matrixVectorMul(RotationXY(angle), rotated.toVector()));
-    rotated = Point4D.toPoint(matrixVectorMul(RotationXZ(angle), rotated.toVector()));
-    rotated = Point4D.toPoint(matrixVectorMul(RotationYZ(angle), rotated.toVector()));
+    rotated = PointND.toPoint(matrixVectorMul(RotationXY(angle), rotated.toVector()));
+    rotated = PointND.toPoint(matrixVectorMul(RotationXZ(angle), rotated.toVector()));
+    rotated = PointND.toPoint(matrixVectorMul(RotationYZ(angle), rotated.toVector()));
   
-    rotated = Point4D.toPoint(matrixVectorMul(RotationXW(angle), rotated.toVector()));
-    rotated = Point4D.toPoint(matrixVectorMul(RotationYW(angle), rotated.toVector()));
-    rotated = Point4D.toPoint(matrixVectorMul(RotationZW(angle), rotated.toVector()));
+    rotated = PointND.toPoint(matrixVectorMul(RotationXW(angle), rotated.toVector()));
+    rotated = PointND.toPoint(matrixVectorMul(RotationYW(angle), rotated.toVector()));
+    rotated = PointND.toPoint(matrixVectorMul(RotationZW(angle), rotated.toVector()));
   
-    let projected3D = Point3D.toPoint(matrixVectorMul(PerspectiveProjection3D(rotated.w), rotated.toVector()));
-    let projected2D = Point2D.toPoint(matrixVectorMul(PerspectiveProjection2D(projected3D.z), projected3D.toVector()));
+    let projected3D = PointND.toPoint(matrixVectorMul(PerspectiveProjection3D(rotated.w), rotated.toVector()));
+    let projected2D = PointND.toPoint(matrixVectorMul(PerspectiveProjection2D(projected3D.z), projected3D.toVector()));
     projected2D.draw(rotated.z, rotated.w);
   }
   requestAnimationFrame(tic);
